a5b7bea1591409de2ae7cdcece48d71f5b8e4fdd
Pawan Upadhyay <141557022+pawansatoshi@users.noreply.github.com>
2026-08-13 02:32:24 +0530

    feat: expose bytecode analysis primitives

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./domain/address.js";
 export * from "./types/analysis.js";
+export * from "./domain/bytecode.js";
 export * from "./infrastructure/config.js";
 export * from "./infrastructure/resilience.js";
 export * from "./infrastructure/rpc.js";
